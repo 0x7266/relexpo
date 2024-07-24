ea04c10d824ba235e58fe68f6ef3a6050050b86f
rafael <rrrr0ffff@gmail.com>
2024-07-24 02:08:50 -0300

    added gradient

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-export default function Home() {
+import beachImage from "@/assets/meditation-images/beach.webp";
+import React from "react";
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="bg-black">Hello, World!</Text>
+    <View className="flex items-center justify-center">
+      <ImageBackground
+        resizeMode="cover"
+        className="flex-1"
+        source={beachImage}
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+        >
+          <SafeAreaView>
+            <Text>Hello, World!</Text>
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
